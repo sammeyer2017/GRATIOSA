4a1a55f01fee6e6b69d4f4540b2994a9be26bfa0
Unknown <r.forquet@gmail.com>
2018-04-25 15:56:38 +0200

    /!\ corriger load TSS quand tag column 0

--- globvar.py
+++ globvar.py
@@ -27,7 +27,7 @@ radius = fig_width / 1.4 # radius circles
 # parameters for bTSS finder
 PROM_LENGTH = 200
 TSS_DOWNSTREAM = 50
-taxon = {'ecoli':'e','dickeya':'e','salmonella':'e'}
+taxon = {'ecoli':'e','dickeya':'e','salmonella':'e', 'synechococcus':'c', 'streptococcus':'c', 'staphylococcus':'c'}
 # parameters for spacer analysis
 sigfactor = 'sigma70'
 spacers = [15,16,17,18,19]
