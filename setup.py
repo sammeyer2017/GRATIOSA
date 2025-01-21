2dc1c4e67cc58aad44e5867600223c06d64de42e
MaiwennP <60888066+MaiwennP@users.noreply.github.com>
2025-01-21 15:19:04 +0100

    PyPI version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="GRATIOSA",
-    version="0.0.3",
+    version="1.0.2",
     author="Pineau Maïwenn, Forquet Raphaël, Meyer Sam",
     author_email="sam.meyer@insa-lyon.fr",
     description=" ",
